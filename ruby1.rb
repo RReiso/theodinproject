8f25879ebf6ebf9b8a9e57de94231f2bc2031dd0
RReiso <rutareiso@protonmail.com>
2021-06-28 14:34:12 -0400

    Class and instant variables example

--- ruby1.rb
+++ ruby1.rb
@@ -37,3 +37,51 @@ end
 puts M::C::D::N::X
 puts M::C::X
 #Output: 1,2
+
+class Painting
+  @@styles = []
+  @@paintings = {}
+  @@total_count = 0
+  attr_reader :style
+  
+  def self.all_paintings
+    @@paintings
+  end
+  def self.total_count
+    @@total_count
+  end
+  def self.add_style_to_library(style)
+    if !@@styles.include?(style)
+      @@styles << style
+      @@paintings[style] = 0
+    end
+  end
+
+  def initialize(style)
+    if @@styles.include?(style)
+      puts "Drawing a new painting in #{style} style!"
+      @style = style
+      @@paintings[style] += 1
+      @@total_count += 1
+    else
+      raise "No such style in the library: #{style}."
+    end
+  end
+  def same_style_count
+    @@paintings[self.style]
+  end
+end
+Painting.add_style_to_library("Impressionism")
+Painting.add_style_to_library("Cubism")
+paint1 = Painting.new("Impressionism")
+paint2 = Painting.new("Cubism")
+paint3 = Painting.new("Impressionism")
+puts "There are #{paint3.same_style_count} paintings in #{paint3.style} style."
+puts "There are #{Painting.total_count} paintigs total."
+puts Painting.all_paintings
+# Drawing a new painting in Impressionism style!
+# Drawing a new painting in Cubism style!
+# Drawing a new painting in Impressionism style!
+# There are 2 paintings in Impressionism style.
+# There are 3 paintigs total.
+# {"Impressionism"=>2, "Cubism"=>1}
