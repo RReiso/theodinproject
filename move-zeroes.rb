7548bffeaf937a0161ee6e7600cbc657fe7a383c
RReiso <rutareiso@protonmail.com>
2021-07-07 19:53:12 -0400

    Second version

--- move-zeroes.rb
+++ move-zeroes.rb
@@ -2,19 +2,31 @@
 # while maintaining the relative order of the non-zero elements.
 # Note that you must do this in-place without making a copy of the array.
 
-def move_zeroes(nums)
+def move_zeroes(nums) #runtime 56ms
   nums.size.times do |num|
     if nums[num]==0
       nums[num]=nil
       nums.push(0)
     end
   end
-  nums.compact!
+   p nums.compact!
 end
 
 move_zeroes([0,1,0,3,12])
-# Output: [1,3,12,0,0]
-# Example 2:
-
 move_zeroes([0])
-# Output: [0]
+
+
+def move_zeroes2(nums) #runtime 68ms
+  counter =0
+  nums.size.times do |num|
+    if nums[num]==0
+      counter+=1
+    end
+  end
+  nums.delete(0)
+  counter.times {nums.push(0)}
+  p nums
+end
+
+move_zeroes2([0,1,0,3,12])
+move_zeroes2([0,0,1])
