e9a4925fdfcc4ca5ee6c45bd2bc6eb043d0923ae
RReiso <rutareiso@protonmail.com>
2021-07-08 20:27:45 -0400

    Bubble sort

--- odinproject/ruby-challenges/bubble-sort.rb
+++ odinproject/ruby-challenges/bubble-sort.rb
@@ -2,18 +2,19 @@
 #a sorted array. It must use the bubble sort methodology 
  
 def bubble_sort(nums)
-  # nums.each_index do |i|
-    # p nums[i], nums[i+1]
-    i=0
+  loop do
+    i = 0
+    swap = false
     until nums[i+1] == nil
       if nums[i] > nums[i+1]
         nums[i],nums[i+1] = nums[i+1], nums[i]
+        swap = true
       end
       i+=1
     end
+    break if swap == false
+  end
     p nums
-  # end
 end
  
-bubble_sort([4,3,78,2,0,2])
-# => [0,2,2,3,4,78]
+bubble_sort([4,3,1,1,17,83,23,0,5,100])
