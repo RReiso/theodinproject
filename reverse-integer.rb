fc68645372a5ff3c40d8748c1f0ddcc397f2d3df
RReiso <rutareiso@protonmail.com>
2021-07-09 20:56:22 -0400

    Array and sum versions with integer division

--- reverse-integer.rb
+++ reverse-integer.rb
@@ -1,4 +1,4 @@
-def reverse(x) #100ms, 210mb
+def reverse(x) 
   int_str = x.to_s
   res_str=String.new
   if int_str[0]=='-'
@@ -17,6 +17,46 @@ def reverse(x) #100ms, 210mb
     0
   end
 end
-reverse(
+reverse(1563847412)
 
-1563847412)
+def reverse2(x) - fastest
+  res = []
+  i = 1
+  if x < 0
+    res<<"-"
+    x*= -1
+  end
+  begin
+    num = x.remainder(i*10)
+    num = num/i
+    res << num
+    i*=10
+  end until x/i==0
+  res = res.join.to_i
+  
+  if res.between?(-2**31,2**31-1)
+    res
+  else
+    0
+  end
+end
+reverse2(-1563847412)
+
+def reverse3(x)
+  res = 0
+  if x<0
+    x=x.abs 
+    neg = true
+  end
+  until x==0
+    res = res * 10 + x.remainder(10);
+    x = x / 10;
+  end
+    res = res * (-1) if neg
+  if res.between?(-2**31,2**31-1)
+    res
+  else
+    0
+  end
+end
+reverse3(1563847412)
