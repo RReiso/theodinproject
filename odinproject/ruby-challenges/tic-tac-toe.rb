d652b0829c92ef9e6cf0d31d78cdbcf36740e3ad
RReiso <rutareiso@protonmail.com>
2021-07-14 00:50:57 -0400

    Draw_board method

--- odinproject/ruby-challenges/tic-tac-toe.rb
+++ odinproject/ruby-challenges/tic-tac-toe.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_reader :marker, :tictactoe
+  attr_reader :marker, :num
 
-  def initialize (tictactoe, marker)
+  def initialize (tictactoe, marker, num)
     @tictactoe = tictactoe
     @marker = marker
+    @num = num
   end
 
   def choose_position
@@ -11,7 +12,7 @@ class Player
       puts "Choose a number from available positions"
       chosen_position = gets.chomp.to_i
       p chosen_position
-      return chosen_position if tictactoe.available_positions.include?(chosen_position)
+      return chosen_position if @tictactoe.available_positions.include?(chosen_position)
     end
   end
 end
@@ -19,38 +20,47 @@ end
 class TicTacToe
   WIN_COMBINATIONS = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
 
-  attr_accessor :current_player
-  attr_reader :available_positions
+  attr_reader :available_positions, :current_player
 
   def initialize
-    @players = [Player.new(self,"x"), Player.new(self, "o")]
+    @players = [Player.new(self,"x", 1), Player.new(self, "o", 2)]
     @current_player = @players[0]
-    @board = %w(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    @board = %w(1 2 3 4 5 6 7 8 9)
     @available_positions = [1,2,3,4,5,6,7,8,9]
   end
 
-  def play(current_player)
+  def play
     loop do
-      # draw_board
-      chosen_position = current_player.choose_position
+      draw_board
+      chosen_position = @current_player.choose_position
       update_available_positions(chosen_position)
-      @board[chosen_position] = current_player.marker
+      @board[chosen_position - 1] = @current_player.marker
       # draw_board
       # if statements
       switch_player
     end
   end
 
+def draw_board
+  divider = "--+---+--"
+  puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+  puts divider
+  puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+  puts divider
+  puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+end
+
   def update_available_positions(pos)
-    available_positions.delete(pos)
-    p @available_positions
+    @available_positions.delete(pos)
+    p "available positions" + @available_positions.to_s
   end
 
   def switch_player
-    self.current_player = self.current_player == @players[0]? @players[1] : @players[0]
+    @current_player = @current_player == @players[0]? @players[1] : @players[0]
+    p "curr_player " + @current_player.num.to_s
   end
 
 end
 
 new_game = TicTacToe.new
-new_game.play(new_game.current_player)
+new_game.play
