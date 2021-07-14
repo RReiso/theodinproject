964c6dad7a9c767d077d9bb52637aa8cafdddd42
RReiso <rutareiso@protonmail.com>
2021-07-14 01:53:28 -0400

    Format code

--- odinproject/ruby-challenges/tic-tac-toe.rb
+++ odinproject/ruby-challenges/tic-tac-toe.rb
@@ -1,7 +1,7 @@
 class Player
   attr_reader :marker, :name
 
-  def initialize (tictactoe, marker, name)
+  def initialize(tictactoe, marker, name)
     @tictactoe = tictactoe
     @marker = marker
     @name = name
@@ -11,23 +11,36 @@ class Player
     loop do
       puts "#{@name}, choose a number from the available positions:"
       chosen_position = gets.chomp.to_i
-      return chosen_position if @tictactoe.available_positions.include?(chosen_position)
+      if @tictactoe.available_positions.include?(chosen_position)
+        return chosen_position
+      end
       puts "\nInvalid input."
     end
   end
 end
 
 class TicTacToe
-  WIN_COMBINATIONS = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
+  WIN_COMBINATIONS = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+  ]
 
   attr_reader :available_positions
 
   def initialize
-    @players = [Player.new(self, "x", "Player_1"), Player.new(self, "o", "Player_2")]
+    @players = [
+      Player.new(self, 'x', 'Player_1'),
+      Player.new(self, 'o', 'Player_2')
+    ]
     @current_player = @players[0]
-    @board = %w(1 2 3 4 5 6 7 8 9)
-    @available_positions = [1,2,3,4,5,6,7,8,9]
-
+    @board = %w[1 2 3 4 5 6 7 8 9]
+    @available_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   def play
@@ -40,6 +53,7 @@ class TicTacToe
       chosen_position = @current_player.choose_position
       update_available_positions(chosen_position)
       @board[chosen_position - 1] = @current_player.marker
+
       if winner?(@current_player)
         draw_board
         sleep(0.5)
@@ -51,12 +65,13 @@ class TicTacToe
         puts "It's a draw!"
         play_again?
       end
+      
       switch_player
     end
   end
 
   def draw_board
-    divider = "--+---+--"
+    divider = '--+---+--'
     puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
     puts divider
     puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
@@ -70,8 +85,8 @@ class TicTacToe
 
   def winner?(player)
     WIN_COMBINATIONS.any? do |combination|
-        combination.all? {|pos| @board[pos - 1] == player.marker}
-      end
+      combination.all? { |pos| @board[pos - 1] == player.marker }
+    end
   end
 
   def draw?
@@ -79,22 +94,21 @@ class TicTacToe
   end
 
   def switch_player
-    @current_player = @current_player == @players[0]? @players[1] : @players[0]
+    @current_player = @current_player == @players[0] ? @players[1] : @players[0]
   end
 
   def play_again?
     loop do
       print "\nPlay again? y/n:"
       answer = gets.chomp.downcase
-      if answer == "n"
+      if answer == 'n'
         exit
-      elsif answer == "y"
+      elsif answer == 'y'
         initialize
         play
       end
     end
   end
-
 end
 
 new_game = TicTacToe.new
