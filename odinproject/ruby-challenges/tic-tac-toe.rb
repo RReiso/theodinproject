881c7422d8bf9ad077fbf5c29f034c7613e06b2b
RReiso <rutareiso@protonmail.com>
2021-07-14 01:23:12 -0400

    Check winner with winner? method

--- odinproject/ruby-challenges/tic-tac-toe.rb
+++ odinproject/ruby-challenges/tic-tac-toe.rb
@@ -1,18 +1,18 @@
 class Player
-  attr_reader :marker, :num
+  attr_reader :marker, :name
 
-  def initialize (tictactoe, marker, num)
+  def initialize (tictactoe, marker, name)
     @tictactoe = tictactoe
     @marker = marker
-    @num = num
+    @name = name
   end
 
   def choose_position
     loop do
-      puts "Choose a number from available positions"
+      puts "#{@name}, choose a number from the available positions:"
       chosen_position = gets.chomp.to_i
-      p chosen_position
       return chosen_position if @tictactoe.available_positions.include?(chosen_position)
+      puts "\nInvalid input."
     end
   end
 end
@@ -20,44 +20,61 @@ end
 class TicTacToe
   WIN_COMBINATIONS = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
 
-  attr_reader :available_positions, :current_player
+  attr_reader :available_positions
 
   def initialize
-    @players = [Player.new(self,"x", 1), Player.new(self, "o", 2)]
+    @players = [Player.new(self, "x", "Player_1"), Player.new(self, "o", "Player_2")]
     @current_player = @players[0]
     @board = %w(1 2 3 4 5 6 7 8 9)
     @available_positions = [1,2,3,4,5,6,7,8,9]
+
   end
 
   def play
+    puts "\nWelcome to Tic Tac Toe game!\n"
+    sleep(2)
     loop do
+      puts "\nGame board:\n\n"
       draw_board
+      sleep(0.5)
       chosen_position = @current_player.choose_position
       update_available_positions(chosen_position)
       @board[chosen_position - 1] = @current_player.marker
       # draw_board
-      # if statements
+      if winner?(@current_player)
+        draw_board
+        sleep(0.5)
+        puts "The winner is #{@current_player.name}!"
+        return
+      # elsif draw?(@current_player)
+      #   puts "It's a draw!"
+      #   return
+      end
       switch_player
     end
   end
 
-def draw_board
-  divider = "--+---+--"
-  puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
-  puts divider
-  puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
-  puts divider
-  puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
-end
+  def draw_board
+    divider = "--+---+--"
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts divider
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts divider
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}\n\n"
+  end
 
   def update_available_positions(pos)
     @available_positions.delete(pos)
-    p "available positions" + @available_positions.to_s
+  end
+
+  def winner?(player)
+    WIN_COMBINATIONS.any? do |combination|
+        combination.all? {|pos| @board[pos - 1] == player.marker}
+      end
   end
 
   def switch_player
     @current_player = @current_player == @players[0]? @players[1] : @players[0]
-    p "curr_player " + @current_player.num.to_s
   end
 
 end
