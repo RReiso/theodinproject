4981f19e55ec7f771fd3d80326280e496a3164ad
RReiso <rutareiso@protonmail.com>
2021-07-23 16:28:16 -0400

    First solution

---
+++ Leetcode/remove-duplicates-from-sorted-list.rb
@@ -0,0 +1,27 @@
+# Remove Duplicates from Sorted List
+# Given the head of a sorted linked list, delete all duplicates
+#  such that each element appears only once. Return the linked list sorted as well.
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+    curr = head.nil? ? head : head.next
+    prev = head
+    while curr
+        if curr.val == prev.val
+            prev.next = curr.next
+        else
+            prev = curr
+        end
+        curr = curr.next
+    end
+    p head
+end
