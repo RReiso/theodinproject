9f333073e3954b1a750a463038c91af6873d140d
RReiso <rutareiso@protonmail.com>
2021-07-23 02:19:10 -0400

    Add Computer class. Add game play logic for computer as code breaker

--- theodinproject/mastermind.rb
+++ theodinproject/mastermind.rb
@@ -1,34 +1,57 @@
 class Human
-  def initialize(all_colors)
-    @all_colors = all_colors
+  def initialize
+    @all_colors = Mastermind::COLORS
   end
 
-  def type_combination
+  def make_guess
     loop do
       puts 'Type your 4 color combination (seperate colors with a space):'
-      combo = gets.chomp.upcase.split(' ')
-      return combo if valid_input?(combo)
+      guess = gets.chomp.upcase.split(' ')
+      return guess if valid_input?(guess)
       puts "\nInvalid input"
     end
   end
 
-  def valid_input?(combination)
-    # all_colors = Mastermind::COLORS
-    combination.size == 4 &&
-      combination.all? { |color| @all_colors.include?(color) }
+  def create_secret_combination
+    loop do
+      puts 'Create a 4 color secret combination (seperate colors with a space):'
+      secret_combination = gets.chomp.upcase.split(' ')
+      return secret_combination if valid_input?(secret_combination)
+      puts "\nInvalid input"
+    end
   end
+
+  private
+  def valid_input?(color_array)
+    color_array.size == 4 &&
+      color_array.all? { |color| @all_colors.include?(color) }
+  end
+end
+
+
+class Computer
+  def initialize
+    
+  end
+
+  def make_guess(turns, all_combinations)
+    if turns == 11
+      %w[YELLOW YELLOW BLUE BLUE]
+  else
+    all_combinations.sample
+  end
+end
 end
 
 class Mastermind
   COLORS = %w[YELLOW BLUE RED GREEN PURPLE WHITE]
 
   def initialize
-    #How to pass COLORS? Or access in Human class -  Mastermind::COLORS
-    @player = Human.new(COLORS)
-    @guesses = 3
+    @human_player = Human.new
     @secret_combination = []
     # Is this necessary?
     # @role = 0
+    # @all_combinations
   end
 
   def play
@@ -40,69 +63,91 @@ class Mastermind
       puts " \nInvalid input!"
     end
     if @role == '1'
-      # player = @players[0]
-      create_secret_combination
+          
+      set_secret_combination
+      turns=3
       p @secret_combination
 
       loop do
-        puts "\nGuesses left: #{@guesses}"
-        @guesses -= 1
-        combination = @player.type_combination
-        print "\nYour combination: "
-        print_combination(combination)
+        puts "\nTurns left: #{turns}"
+        turns -= 1
+        human_guess = @human_player.make_guess
+        print "\nYour guess: "
+        print_combination(human_guess)
 
-        if winner?(combination)
+        if winner?(human_guess)
           puts yellow('Congratulations! You won!')
           play_again?
-        elsif @guesses == 0
-          puts red("\nNo more guesses. You lost!")
+        elsif turns == 0
+          puts red("\nNo more turns. You lost!")
           puts 'The secret combination was: '
           print_combination(@secret_combination)
           play_again?
         else
-          clues = check_for_clues(combination)
+          clues = check_for_clues(human_guess)
           print_clues(clues)
         end
       end
+    else
+      computer_player = Computer.new
+      @all_combinations = Mastermind::COLORS.repeated_permutation(4).to_a
+      @secret_combination = @human_player.create_secret_combination
+      turns = 12
+loop do
+  turns -= 1
+  computer_guess = computer_player.make_guess(turns, @all_combinations)
+  p "comp guess #{computer_guess}"
+  
+if winner?(computer_guess)
+          puts red('Computer won! You lost!')
+          play_again?
+elsif turns == 0
+    puts yellow('Congratulations! You won! Computer could not break your code')
+          play_again?
+else
+clues = check_for_clues(computer_guess.dup)
+  print_clues(clues)
+
+  find_remaining_combinations(computer_guess, clues)
+  
+end
     end
   end
+end
 
-  def create_secret_combination
+  def set_secret_combination
     4.times { @secret_combination << COLORS.sample }
   end
 
-  def print_combination(combination)
-    combination.each { |color| print "#{color} " }
+  def print_combination(guess)
+    guess.each { |color| print "#{color} " }
     puts "\n"
   end
 
-  def winner?(combination)
-    combination == @secret_combination
+  def winner?(guess)
+    guess == @secret_combination
   end
 
-  def check_for_clues(combination)
+  def check_for_clues(guess)
     clues = []
-
     secret_combo = @secret_combination.dup
 
     # Find and mark exact matches
     secret_combo.each_with_index do |color, i|
-      if color == combination[i]
+      if color == guess[i]
         secret_combo[i] = 'exact match'
-        combination[i] = 'X'
+        guess[i] = 'X'
         clues << red("\u26AB".encode('utf-8'))
       end
     end
 
     # Find and mark color matches
     secret_combo.each_with_index do |color, i|
-      if combination.include?(color)
+      if guess.include?(color)
         clues << "\u26AB".encode('utf-8')
-        combination[combination.index(color)] = 'X'
+        guess[guess.index(color)] = 'X'
       end
     end
-    # p secret_combo
-    # p combination
     clues
   end
 
@@ -110,6 +155,96 @@ class Mastermind
     print 'Clues: '
     clues.each { |clue| print clue }
     puts "\n"
+  end
+
+  def find_remaining_combinations(computer_guess, clues)
+    red_pegs, white_pegs = nr_of_pegs(clues)
+    
+    p red_pegs
+    p white_pegs
+    if red_pegs
+      p "in red"
+      possible_matching_positions = [0, 1, 2, 3].combination(red_pegs).to_a
+      remaining_combinations = check_red_pegs(possible_matching_positions, computer_guess)
+    
+    elsif white_pegs
+      "in White"
+      possible_matching_colors = computer_guess.combination(white_pegs).to_a.uniq
+      remaining_combinations = check_white_pegs(possible_matching_colors, computer_guess)
+    else
+      p "in no"
+      # no red pegs no white pegs. need to delete all combinations with the colors form the guess
+      colors_not_in_secret_code = computer_guess.tally.keys
+      remaining_combinations = delete_combinations(colors_not_in_secret_code)
+    end
+    remaining_combinations.delete(computer_guess)
+    @all_combinations = remaining_combinations
+    p @all_combinations
+
+  end
+
+def nr_of_pegs(clues)
+    clues = clues.tally
+    p clues
+  red_pegs = clues["\e[31m⚫\e[0m"] #nil if none
+  white_pegs = clues["⚫"]
+  return red_pegs, white_pegs
+end
+
+def check_red_pegs(possible_matching_positions, computer_guess)
+  p possible_matching_positions
+  remaining_combinations = []
+@all_combinations.each_with_index do |combination, i|
+      possible_matching_positions.each do |array_of_positions|
+       
+        match = []
+        array_of_positions.each do |position|
+            p combination[position], computer_guess[position]
+          combination[position] == computer_guess[position] ? match << '+' : match << '-'
+        end
+
+        if match.all? { |el| el == '+' }
+          # p combination
+          remaining_combinations << combination
+          break
+        end
+      end
+    end
+    remaining_combinations
+  end
+
+  def check_white_pegs(possible_matching_colors,computer_guess)
+    remaining_combinations=[]
+    @all_combinations.each_with_index do |combination, i|
+      possible_matching_colors.each do |array_of_colors|
+        match = []
+        array_of_colors.each do |color|
+          combination.include?(color) ? match << '+' : match << '-'
+        end
+        if match.all? { |el| el == '+' }
+          remaining_combinations << combination
+          break
+        end
+      end
+    end
+    remaining_combinations
+  end
+
+  def delete_combinations(colors_not_in_secret_code)
+    p "Problem"
+    # p @all_combinations
+    remaining_combinations=[]
+    @all_combinations.each_with_index do |combination, i|
+      colors_not_in_secret_code.each do |color|
+        if combination.include?(color)
+          @all_combinations[i] = nil
+          break # break if found one match, no need to go through all
+        end
+      end
+    end
+    # p @all_combinations
+    remaining_combinations = @all_combinations.compact
+    remaining_combinations
   end
 
   def play_again?
@@ -138,105 +273,7 @@ class Mastermind
   end
 end
 
-# new_game = Mastermind.new
-# new_game.play
+new_game = Mastermind.new
+new_game.play
 
 
-
-#COMPUTER LOGIC
-   def check_for_clues(combination)
-  clues = []
-  secret_combo = %w[BLUE YELLOW GREEN RED].dup
-
-  # Find and mark exact matches
-  secret_combo.each_with_index do |color, i|
-    if color == combination[i]
-      secret_combo[i] = 'exact match'
-      combination[i] = 'X'
-      clues << 'red'
-    end
-  end
-
-  # Find and mark color matches
-  secret_combo.each_with_index do |color, i|
-    if combination.include?(color)
-      clues << 'white'
-      combination[combination.index(color)] = 'X'
-    end
-  end
-  clues
-end
-
-all_combinations =
-  %w[YELLOW BLUE RED GREEN PURPLE WHITE].repeated_permutation(4).to_a
-count = 0
-loop do
-  count += 1
-  if count == 1
-    guess = %w[YELLOW YELLOW BLUE BLUE]
-  else
-    guess = all_combinations.sample
-  end
-
-  p "comp guess #{guess}"
-  clues = check_for_clues(guess.dup)
-  clues = clues.tally
-  p clues
-  red_pegs = clues['red'] #nil if none
-  white_pegs = clues['white']
-
-  if red_pegs == 4
-    p guess
-    p count
-    exit
-  end
-
-  remaining_combinations = []
-  
-  if red_pegs
-    possible_matching_positions = [0, 1, 2, 3].combination(red_pegs).to_a
-
-    all_combinations.each_with_index do |combination, i|
-      possible_matching_positions.each do |array_of_positions|
-        match = []
-        array_of_positions.each do |position|
-          combination[position] == guess[position] ? match << '+' : match << '-'
-        end
-
-        if match.all? { |el| el == '+' }
-          remaining_combinations << combination
-          break
-        end
-      end
-    end
-  elsif white_pegs
-    possible_matching_colors = guess.combination(white_pegs).to_a.uniq
-    all_combinations.each_with_index do |combination, i|
-      possible_matching_colors.each do |array_of_colors|
-        match = []
-        array_of_colors.each do |color|
-          combination.include?(color) ? match << '+' : match << '-'
-        end
-        if match.all? { |el| el == '+' }
-          remaining_combinations << combination
-          break
-        end
-      end
-    end
-  else
-    # no red pegs no white pegs. need to delete all combinations with the colors form the guess
-    colors_not_in_secret_code = guess.tally.keys
-    all_combinations.each_with_index do |combination, i|
-      colors_not_in_secret_code.each do |color|
-        if combination.include?(color)
-          all_combinations[i] = nil
-          counter += 1
-          break # break if found one match, no need to go through all
-        end
-      end
-    end
-    remaining_combinations = all_combinations.compact
-  end
-  remaining_combinations.delete(guess)
-  all_combinations = remaining_combinations
-end
