4ee01c27820b72591fc38c8d08490a17e7711768
RReiso <rutareiso@protonmail.com>
2021-07-30 20:38:22 -0400

    Adjust spaces

--- theodinproject/hangman/dictionary.rb
+++ theodinproject/hangman/dictionary.rb
@@ -8,15 +8,16 @@ class Dictionary
     @eligible_words.sample
   end
 
-
-private
+  private
 
   def eligible_words
     eligible_words = []
-    File.readlines(@word_file).each do |word|
-     word.gsub!(/\r\n?/, "")
-     eligible_words << word if word.size > 4 && word.size < 13
-    end
+    File
+      .readlines(@word_file)
+      .each do |word|
+        word.gsub!(/\r\n?/, '')
+        eligible_words << word if word.size > 4 && word.size < 13
+      end
     eligible_words
   end
 end
