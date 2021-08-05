35068aa5b5f9e1d101744f3485c65cb216021b61
Ruta R <rutareiso@protonmail.com>
2021-08-05 01:57:56 -0400

    Refactor code

--- theodinproject/custom-enumerables/enumerable.rb
+++ theodinproject/custom-enumerables/enumerable.rb
@@ -9,61 +9,50 @@ module Enumerable
     index = 0
     for el in self
       yield el, index
-      index+=1
+      index += 1
     end
   end
 
   def my_select
-    result = []
+    result = is_a?(Hash) ? {} : []
     for el in self
-      result << el if yield el
+      if yield el
+        is_a?(Hash) ? result.store(el[0], el[1]) : result << el
+      end
     end
     result
   end
 
   def my_all?
-    self.my_each do |el|
-      return false unless yield el
-    end
+    my_each { |el| return false unless yield el }
     true
   end
 
   def my_any?
-     self.my_each do |el|
-      return true if yield el
-    end
+    my_each { |el| return true if yield el }
     false
   end
 
   def my_none?
-    self.my_each do |el|
-      return false if yield el
-    end
+    my_each { |el| return false if yield el }
     true
   end
 
   def my_count
     counter = 0
-    self.my_each do |el|
-      counter+=1 if yield el
-    end
+    my_each { |el| counter += 1 if yield el }
     counter
   end
 
-  def my_map
+   def my_map(my_proc = nil)
     array = []
-    self.my_each {|el| array << (yield el)}
+    my_each { |el| array << (my_proc ? my_proc.call(el) : (yield el)) }
     array
   end
 
   def my_inject(start_value)
     acc = start_value
-    self.my_each do |el|
-      acc = (yield acc, el)
-    end
+    my_each { |el| acc = (yield acc, el) }
     acc
   end
 end
-numbers = [1, 2, 3, 4]
-proc = Proc.new {|el| el * 100}
-p numbers.my_map(&proc) 
