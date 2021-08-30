de13e912e0d8a700991ad5edf605bd3024379764
Ruta R <rutareiso@protonmail.com>
2021-08-29 23:13:35 -0400

    Refactor code

--- theodinproject/knight_travails/knight.rb
+++ theodinproject/knight_travails/knight.rb
@@ -1,86 +1,35 @@
-
 class Knight
-  DIRECTIONS = [[1,2],[2,1],[2,-1],[-1,2],[1,-2],[-2,1],[-1,-2],[-2,-1]]
+  DIRECTIONS = [
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [-1, 2],
+    [1, -2],
+    [-2, 1],
+    [-1, -2],
+    [-2, -1]
+  ].freeze
 
-  attr_reader :possible_moves,  :current_position
-  attr_accessor :parent, :child_nodes
+  attr_reader :current_position, :parent
+  attr_accessor :child_nodes
 
-  def initialize(current_position, end_position, parent=nil)
+  def initialize(current_position, end_position, parent = nil)
     @current_position = current_position
     @end_position = end_position
     @parent = parent
     @child_nodes = []
-    
-  end
-  
-  def play
-    
-    
-    # @possible_moves = find_possible_moves 
-    parent = find_parent_of_ending_node
-    get_path(parent, @current_position)
-    # p self.parent
-    # p "visited positions #{$visited_positions}"
   end
 
   def find_possible_moves
-   
-possible_moves = []
-# p " curr pos #{@current_position}"
-DIRECTIONS.each do |x_coordinate, y_coordinate|
-  current = @current_position.dup
-  current[0] += x_coordinate
-  current[1] += y_coordinate
-  next if current[0].negative? || current[1].negative? || current[0]>8 ||current[1]>8
-  possible_moves << current
-end
-# p "poss moves #{possible_moves}"
-possible_moves
-end
+    possible_moves = []
+    DIRECTIONS.each do |x_coordinate, y_coordinate|
+      current = @current_position.dup
+      current[0] += x_coordinate
+      current[1] += y_coordinate
+      next if current[0].negative? || current[1].negative? || current[0] > 8 || current[1] > 8
 
-def find_parent_of_ending_node
-
-  queue = [self]
-    node = queue.shift
-    while queue
-    
-      node.find_possible_moves.each do |position|
- 
-        if position != @end_position
-          node.child_nodes << a = Knight.new(position, @end_position,node)
-          queue<<a
-        else
-          return node
-
-      
+      possible_moves << current
     end
-
+    possible_moves
   end
-  node=queue.shift
-  
 end
-end
-
-
-def get_path(node,start_position )
-# Given node's child is the end_position
-  path =[@end_position]
-  while node.current_position !=start_position
-    path.unshift(node.current_position)
-    node = node.parent
-  end
-  path.unshift(start_position)
-  p path
-  # exit
-end
-
-end
-
-knight = Knight.new([0,3],[8,7])
-# p knight
-# p "I am #{knight} "
-knight.play
-# 
-
-
-
